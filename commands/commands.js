dd5bf919f5d1909ba6fbf5af56ebe0ab2882b573
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2020-08-26 19:42:44 +0300

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'list of commands',
     execute(message, args){
         console.log('!commands called');
-        let embed = new Discord.MessageEmbed().addField('List of available commands :',`!play <YT link> - places a song into the queue \n 
+        let embed = new Discord.MessageEmbed().addField('List of available commands :',`!play <YT video/playlist link> - places a song/playlist into the queue \n 
         !skip - skips the current song \n 
         !stop - stops the music \n 
         !queue - posts current music queue \n
@@ -14,6 +14,7 @@ module.exports = {
         !anime - random anime \n 
         !roll <value> - random number between 1 and your value \n
         !peko - just don't. \n 
+        !hit <@someone> - hit someone with a giant halberd \n
         !ping <> - pong!`);
         message.author.send(embed);
     }
